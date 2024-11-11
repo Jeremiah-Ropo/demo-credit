e37b7b5282739195569984c4382ce4bb28d9e4e2
Jeremiah-Ropo <patrickjerry59@yahoo.com>
2024-11-11 08:28:25 +0100

    chores: update knexfile

--- knexfile.ts
+++ knexfile.ts
@@ -4,6 +4,25 @@ import { DATABASE_URL } from './src/config';
 const config: { [key: string]: Knex.Config } = {
     development: {
         client: 'mysql2',
+        connection: {
+            host: '127.0.0.1',
+            user: 'root',
+            database: 'demo_credit',
+            password: 'jeremiah'
+        },
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            directory: './src/models/knex/migrations'
+        },
+        seeds: {
+            directory: './src/models/knex/seeds'
+        },
+    },
+    production: {
+        client: 'mysql2',
         connection: `${DATABASE_URL}`,
         pool: {
             min: 2,
@@ -15,8 +34,7 @@ const config: { [key: string]: Knex.Config } = {
         seeds: {
             directory: './src/models/knex/seeds'
         },
-        
     }
-}
+};
 
 export default config;
