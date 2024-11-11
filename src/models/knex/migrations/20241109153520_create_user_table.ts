44566e1ad556690e9efabee8ead9c38225b729ca
Jeremiah-Ropo <patrickjerry59@yahoo.com>
2024-11-11 12:28:16 +0100

    chores: just a comment

--- src/models/knex/migrations/20241109153520_create_user_table.ts
+++ src/models/knex/migrations/20241109153520_create_user_table.ts
@@ -1,13 +1,13 @@
-import { Knex } from 'knex';
+// import { Knex } from 'knex';
 
-exports.up = function (knex) {
-  return knex.schema.table('Users', function (table) {
-    table.boolean('blackListed').defaultTo(false);
-  });
-};
+// exports.up = function (knex) {
+//   return knex.schema.table('Users', function (table) {
+//     table.boolean('blackListed').defaultTo(false);
+//   });
+// };
 
-exports.down = function (knex) {
-  return knex.schema.table('Users', function (table) {
-    table.dropColumn('blackListed');
-  });
-};
+// exports.down = function (knex) {
+//   return knex.schema.table('Users', function (table) {
+//     table.dropColumn('blackListed');
+//   });
+// };
