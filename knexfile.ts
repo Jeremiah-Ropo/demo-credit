76a92da3fdc6a846ea9bd4b8569aaa3f29a04bfd
Jeremiah-Ropo <patrickjerry59@yahoo.com>
2024-11-11 11:11:31 +0100

    fix: checking it out

--- knexfile.ts
+++ knexfile.ts
@@ -2,44 +2,25 @@ import { Knex } from 'knex';
 import 'dotenv/config';
 
 const config: { [key: string]: Knex.Config } = {
-    development: {
-        client: 'mysql2',
-        connection: {
-            host: '127.0.0.1',
-            user: 'root',
-            database: 'demo_credit',
-            password: 'jeremiah'
-        },
-        pool: {
-            min: 2,
-            max: 10
-        },
-        migrations: {
-            directory: './src/models/knex/migrations'
-        },
-        seeds: {
-            directory: './src/models/knex/seeds'
-        },
+  development: {
+    client: 'mysql2',
+    connection: {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
     },
-    production: {
-        client: 'mysql',
-        connection: {
-            host: process.env.DB_HOST,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASS,
-            database: process.env.DB_NAME,
-        },
-        pool: {
-            min: 2,
-            max: 10
-        },
-        migrations: {
-            directory: './src/models/knex/migrations'
-        },
-        seeds: {
-            directory: './src/models/knex/seeds'
-        },
-    }
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: './src/models/knex/migrations',
+    },
+    seeds: {
+      directory: './src/models/knex/seeds',
+    },
+  },
 };
 
 export default config;
