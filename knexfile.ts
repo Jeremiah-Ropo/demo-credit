2b75b0b2217ee53f67e932de7c76cc04d8600952
Jeremiah-Ropo <patrickjerry59@yahoo.com>
2024-11-11 12:16:20 +0100

    fix: fix knexfile

--- knexfile.ts
+++ knexfile.ts
@@ -1,27 +1,9 @@
+import { Knex } from 'knex';
 import 'dotenv/config';
 
-module.exports = {
-  development: {
-    client: 'mysql2',
-    connection: {
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME,
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      directory: './src/models/knex/migrations',
-    },
-    seeds: {
-      directory: './src/models/knex/seeds',
-    },
-    },
-    production: {
-        client: 'mysql',
+const config: { [key: string]: Knex.Config } = {
+    development: {
+        client: 'mysql2',
         connection: {
             host: process.env.DB_HOST,
             user: process.env.DB_USER,
@@ -29,15 +11,35 @@ module.exports = {
             database: process.env.DB_NAME,
         },
         pool: {
-          min: 2,
-          max: 10,
+            min: 2,
+            max: 10,
         },
         migrations: {
-          directory: './src/models/knex/migrations',
+            directory: './src/models/knex/migrations',
         },
         seeds: {
-          directory: './src/models/knex/seeds',
+            directory: './src/models/knex/seeds',
         },
-      }
+    },
+    production: {
+        client: 'mysql2',
+        connection: {
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASS,
+            database: process.env.DB_NAME,
+        },
+        pool: {
+            min: 2,
+            max: 10,
+        },
+        migrations: {
+            directory: './src/models/knex/migrations',
+        },
+        seeds: {
+            directory: './src/models/knex/seeds',
+        },
+    },
 };
 
+export default config;
--- src/utils/connection.ts
+++ src/utils/connection.ts
@@ -1,6 +1,7 @@
 import { knex } from 'knex';
+import 'dotenv/config';
 import config from '../../knexfile';
 
-const db = knex(config.development);
-console.log('db');
+const db = knex(config[process.env.NODE_ENV || 'development']);
+console.log("db")
 export { db };
