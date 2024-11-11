5c8e81d8773f3e40a3d14ce2d7bcf8aa6015d602
Jeremiah-Ropo <patrickjerry59@yahoo.com>
2024-11-11 10:17:21 +0100

    chores: changes on knexfile

--- knexfile.ts
+++ knexfile.ts
@@ -1,6 +1,5 @@
 import { Knex } from 'knex';
 import 'dotenv/config';
-import { DATABASE_URL } from './src/config';
 
 const config: { [key: string]: Knex.Config } = {
     development: {
@@ -24,7 +23,7 @@ const config: { [key: string]: Knex.Config } = {
     },
     production: {
         client: 'mysql2',
-        connection: `mysql2://root:jeremiah@127.0.0.1:3306/demo_credit`,
+        connection: process.env.DATABASE_URL || 'mysql2://root:jeremiah@127.0.0.1:3306/demo_credit',
         pool: {
             min: 2,
             max: 10
