93011982761a0034713c4b1cd82b0ec005eb76ba
Jeremiah-Ropo <patrickjerry59@yahoo.com>
2024-11-11 10:57:23 +0100

    fix: fixing knexfile

--- knexfile.ts
+++ knexfile.ts
@@ -23,12 +23,7 @@ const config: { [key: string]: Knex.Config } = {
     },
     production: {
         client: 'mysql2',
-        connection:  {
-            host: "127.0.0.1",
-            user: 'root',
-            password: 'jeremiah',
-            database:"demo_credit",
-        },
+        connection:  `${process.env.DATABASE_URL}`,
         pool: {
             min: 2,
             max: 10
