f12009b1d03b16ca190053e606aa4d80c9d2f93d
Jeremiah-Ropo <patrickjerry59@yahoo.com>
2024-11-11 09:14:10 +0100

    fix: knex config

--- knexfile.ts
+++ knexfile.ts
@@ -2,26 +2,26 @@ import { Knex } from 'knex';
 import { DATABASE_URL } from './src/config';
 
 const config: { [key: string]: Knex.Config } = {
+    // development: {
+    //     client: 'mysql2',
+    //     connection: {
+    //         host: '127.0.0.1',
+    //         user: 'root',
+    //         database: 'demo_credit',
+    //         password: 'jeremiah'
+    //     },
+    //     pool: {
+    //         min: 2,
+    //         max: 10
+    //     },
+    //     migrations: {
+    //         directory: './src/models/knex/migrations'
+    //     },
+    //     seeds: {
+    //         directory: './src/models/knex/seeds'
+    //     },
+    // },
     development: {
-        client: 'mysql2',
-        connection: {
-            host: '127.0.0.1',
-            user: 'root',
-            database: 'demo_credit',
-            password: 'jeremiah'
-        },
-        pool: {
-            min: 2,
-            max: 10
-        },
-        migrations: {
-            directory: './src/models/knex/migrations'
-        },
-        seeds: {
-            directory: './src/models/knex/seeds'
-        },
-    },
-    production: {
         client: 'mysql2',
         connection: `${DATABASE_URL}`,
         pool: {
